b49c1e0c8d2fe4d20bb40bee762339db41a0e3cb
choi-jaehyung <writer.jaehyung@gmail.com>
2026-02-01 23:00:20 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/fairtoon-site/', // 본인의 리포지토리 이름을 넣으세요
+  base: '/fairtoon-site/', 
 })
